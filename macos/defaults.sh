150f558b22a3bb23a3c0d1a89bc6545b136b05eb
Kigou-No1 <hadzukichan@proton.me>
2023-11-23 15:53:36 +0900

    edit defaults

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -1,10 +1,12 @@
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool TRUE
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool TRUE
-sudo defaults write bluetoothaudiod "Enable AptX codec" -bool true
-sudo defaults write bluetoothaudiod "Enable AAC codec" -bool true
+defaults write bluetoothaudiod "Enable AptX codec" -bool true
+defaults write bluetoothaudiod "Enable AAC codec" -bool true
 defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
 defaults write com.apple.finder AppleShowAllFiles YES
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 defaults write NSGlobalDomain InitialKeyRepeat -int 15
 defaults write NSGlobalDomain KeyRepeat -int 2
 defaults write com.apple.TextEdit RichText -int 0
+defaults write -g ApplePressAndHoldEnabled -bool false
+
