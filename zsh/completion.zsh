8dfac2f8d8ce535d6159d8f6c3a3bdd9a5080163
Kigou-No1 <hadzukichan@proton.me>
2023-11-23 15:01:48 +0900

    rm nvm

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -6,11 +6,7 @@ then
   compinit
 fi
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-## 補完で小文字でも大文字にマッチさせる
+# 補完で小文字でも大文字にマッチさせる
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 ## 補完候補を一覧表示したとき、Tabや矢印で選択できるようにする
