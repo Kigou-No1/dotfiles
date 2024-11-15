a98d5f3f57d65889ff5bfb114988becd7910a2ac
Kigou-No1 <53385458+Kigou-No1@users.noreply.github.com>
2024-11-15 12:44:52 +0900

    add zsh

---
+++ dot_zsh/options.zsh
@@ -0,0 +1,26 @@
+HISTFILE=~/.zsh/.zsh_history
+HISTSIZE=100000
+SAVEHIST=100000
+setopt append_history
+setopt share_history
+setopt hist_ignore_all_dups
+
+alias hh=hstr                    # hh to be alias for hstr
+setopt histignorespace           # skip cmds w/ leading space from history
+export HSTR_CONFIG=hicolor       # get more colors
+bindkey -s "\C-r" "\C-a hstr -- \C-j"     # bind hstr to Ctrl-r (for Vi mode check doc)
+export HSTR_TIOCSTI=y
+
+. "$HOME/.cargo/env" # load cargo
+export EDITOR='code-insiders'
+export GPG_TTY=$(tty)
+
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+[ -s "/home/hadzuki/.bun/_bun" ] && source "/home/hadzuki/.bun/_bun"
+
+# 補完で小文字でも大文字にマッチさせる
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+# 補完候補を一覧表示したとき、Tabや矢印で選択できるようにする
+zstyle ':completion:*:default' menu select=1
+autoload -U compinit; compinit
