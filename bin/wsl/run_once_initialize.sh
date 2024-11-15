e953186443c15e1401abf8093f139eb26a4998b0
Kigou-No1 <53385458+Kigou-No1@users.noreply.github.com>
2024-11-15 12:44:15 +0900

    remove unused packages

--- bin/wsl/run_once_initialize.sh
+++ bin/wsl/run_once_initialize.sh
@@ -9,10 +9,9 @@ sudo sed -i 's|http[s]*://[a-zA-Z0-9./-]*/ubuntu|https://mirrors.cat.net/ubuntu|
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install asdf bat build-essential \
                 cloudflared curl docker fd \
-                fzf gcc gh git gnupg g++ htop \
-                jq libtool make neofetch ripgrep \
-                tmux tree unzip wget zoxide zsh \
-                zsh-completions -y
+                fzf gcc gh git gnupg g++ jq\
+                libtool make neofetch ripgrep \
+                tmux tree unzip wget zoxide zsh -y
 
 # Add authorized_keys
 # curl -s https://github.com/Kigou-No1.keys > ~/.ssh/authorized_keys
@@ -29,6 +28,8 @@ asdf global nodejs latest
 asdf global python latest
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y # Install rust silently
+
+cargo install sheldon # Install Zsh package manager
 
 mkdir -p ~/repo/github.com/Kigou-No1
 
