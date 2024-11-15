323e45393a6c12ccdd21265ed01cbafce9704dea
Kigou-No1 <53385458+Kigou-No1@users.noreply.github.com>
2024-11-15 14:16:36 +0900

    manage starship and chezmoi with asdf

--- bin/asdf.sh
+++ bin/asdf.sh
@@ -1,8 +1,13 @@
 asdf plugin-add nodejs
 asdf plugin-add python
+asdf plugin-add chezmoi 
+asdf plugin-add starship
+
 
 asdf install nodejs latest
 asdf install python latest
+asdf install chezmoi 2.54.0
 
 asdf global nodejs latest
 asdf global python latest
+asdf global chezmoi 2.54.0
