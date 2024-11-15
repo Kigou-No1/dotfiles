9a7681f7b9b18d1084b437ea039da6d1eddb3e3c
Kigou-No1 <53385458+Kigou-No1@users.noreply.github.com>
2024-11-15 14:23:18 +0900

    install starship with asdf

--- bin/asdf.sh
+++ bin/asdf.sh
@@ -7,7 +7,9 @@ asdf plugin-add starship
 asdf install nodejs latest
 asdf install python latest
 asdf install chezmoi 2.54.0
+asdf install starship latest
 
 asdf global nodejs latest
 asdf global python latest
 asdf global chezmoi 2.54.0
+asdf global starship latest
