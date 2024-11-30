510565b363dce309ab29ee05cc8dae123e16744b
Kigou-No1 <53385458+Kigou-No1@users.noreply.github.com>
2024-11-30 22:35:50 +0900

    a

--- dot_config/powershell/Microsoft.PowerShell_profile.ps1
+++ dot_config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
 $ENV:STARSHIP_CONFIG = "$HOME\.config\starship\starship.toml"
 $ENV:FZF_DEFAULT_OPTS="--height 40% --border --reverse "
 $ENV:EDITOR="code-insiders"
-$scripts = ".config/powershell/scripts"
+$scripts = "$HOME/.config/powershell/scripts"
 Get-ChildItem $scripts -Filter "*.ps1" | ForEach-Object { . $_.FullName }
